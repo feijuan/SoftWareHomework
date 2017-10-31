5c543f4c8defce784a6d813cfcaed11a9a52ee5f
feijuan <32636019+feijuan@users.noreply.github.com>
2017-10-31 11:24:07 +0800

    删除旧代码

--- 源码/SmartHome/src/sh/ssm/dao/SensorManageMapper.java
+++
@@ -1,9 +0,0 @@
-package sh.ssm.dao;
-
-import sh.ssm.po.SensorManage;
-
-public interface SensorManageMapper {
-    int insert(SensorManage record);
-
-    int insertSelective(SensorManage record);
-}
